bebb0b4087413de53164d7b41fd1811614650c3f
Tin Trinh <ttrinh25@my.bcit.ca>
2026-03-14 13:33:51 -0700

    I'm playing around

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,27 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const NUMBER_OF_CHARS = 12;
+
+type Character = {
+	name: string;
+	chance: number;
+	img: string;
+	quantity: number;
+}
+
+let userCollection = new Object({
+	
+});
+
+
+let playerData = {
+	point: 0,
+	coins: 0,
+	collection: userCollection
+}
+
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -17,10 +38,32 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from GachaAmi!');
+
+		// For when the user is typing 
+		vscode.workspace.onDidChangeTextDocument(async change => {
+			let textDoc = change.document;
+			let text = textDoc.getText();
+			console.log(text);
+			console.log(textDoc.fileName);
+			context.globalState.update("textCount", text.length);
+		})
+
+		
 	});
 
+
+	const check = vscode.commands.registerCommand('gachaami.checkData', () => {
+		let textCount = context.globalState.get("textCount");
+		if (textCount) {
+			console.log(textCount);
+		} else {
+			console.log("none");
+		}
+	})
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(check);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
